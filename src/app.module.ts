48259a487e5515e417296c73e3195b6cdafbaae2
Duy-Kun <sherlockduy2001@gmail.com>
2025-05-06 00:00:10 +0200

    dev: add comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-
+//
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
