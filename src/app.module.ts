9976246468aad469d961bfa3e32eeda811933787
Duy-Kun <sherlockduy2001@gmail.com>
2025-05-07 15:36:21 +0200

    hidden db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { DatabaseModule } from './database/database.module';
+// import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
-    DatabaseModule,
+    // DatabaseModule,
   ],
   controllers: [],
   providers: [],
