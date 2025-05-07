348f001ba22e2e30e68bed3cf3cd3452035e2510
Duy-Kun <sherlockduy2001@gmail.com>
2025-05-07 15:49:41 +0200

    debug

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const port = 3001;
 
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    allowedHeaders: 'Content-Type, Access-Control-Allow-Headers, Authorization',
-    credentials: true,
-  });
-  app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(new ValidationPipe());
+  // app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3001);
   logger.log(`Application listening on port ${port}`);
 }
