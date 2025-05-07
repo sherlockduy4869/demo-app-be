218007426b4cdaf9be042ede69e08df0b9e799b7
Duy-Kun <sherlockduy2001@gmail.com>
2025-05-07 15:58:00 +0200

    revert to original

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 // import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    UserModule,
+    // ConfigModule.forRoot({ isGlobal: true }),
+    // UserModule,
     // DatabaseModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
