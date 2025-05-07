32c65f00c019a04c3763fa0b52137740fc05a61d
Duy-Kun <sherlockduy2001@gmail.com>
2025-05-07 16:21:58 +0200

    new debug

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(id: number): string {
+    return `This action returns a #${id} user`;
   }
 }
