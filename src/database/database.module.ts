3fb368eb4285b1421e1f3ad09339418835249f96
Duy-Kun <sherlockduy2001@gmail.com>
2025-05-09 16:27:56 +0200

    new db connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,13 +7,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: 'db-demo-app.c9qq4goo01qb.eu-north-1.rds.amazonaws.com',
+        host: 'database-2.c9qq4goo01qb.eu-north-1.rds.amazonaws.com',
         port: 3306,
         username: 'admin',
         password: 'Conan12#456',
         database: 'db_demo_app',
-        // autoLoadEntities: true,
-        // synchronize: true, // Use only in development
+        autoLoadEntities: true,
+        synchronize: true, // Use only in development
       }),
       inject: [ConfigService],
     }),
