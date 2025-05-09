d07fe4cf41925e0ee864b4f4cc67661c1767d229
Duy-Kun <sherlockduy2001@gmail.com>
2025-05-09 23:52:28 +0200

    change db

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: 'database-3-test.c9qq4goo01qb.eu-north-1.rds.amazonaws.com',
+        host: 'db-demo-app-final.c9qq4goo01qb.eu-north-1.rds.amazonaws.com',
         port: 3306,
         username: 'admin',
         password: 'Conan12#456',
