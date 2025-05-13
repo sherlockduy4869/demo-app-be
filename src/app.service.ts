091dee54a478b553f2fbe87596cc841862cc889e
Duy-Kun <sherlockduy2001@gmail.com>
2025-05-13 15:18:53 +0200

    new test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,22 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Duy from demo-app-be!';
+    try {
+      console.log('Chay ok roi ne');
+      return 'Hello Duy from demo-app-be!';
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  healthCheck(): string {
+    try {
+      console.log('healthCheck ok nha');
+      return 'Health check ok roi';
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
   }
 }
