cb2bbe406d638a1da7c9463e0835d17f55d16316
Duy-Kun <sherlockduy2001@gmail.com>
2025-05-23 20:47:01 +0200

    dev: new db connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: 'database-2.c9qq4goo01qb.eu-north-1.rds.amazonaws.com',
+        host: 'database-3.c9qq4goo01qb.eu-north-1.rds.amazonaws.com',
         port: 3306,
         username: 'admin',
         password: 'Conan12#456',
